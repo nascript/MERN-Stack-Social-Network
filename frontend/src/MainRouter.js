23b20b6b5e19440992faf4bd95a8f41bf9b5adbb
nascript <nascodefy@gmail.com>
2022-02-06 23:17:43 +0700

    homepage

--- frontend/src/MainRouter.js
+++ frontend/src/MainRouter.js
@@ -34,6 +34,7 @@ const MainRouter = () => (
       <Route exact path='/users' component={Users} /> */}
       <Route exact path='/signup' component={Signup} />
       <Route exact path='/signin' component={Signin} />
+      <Route exact path='/signin' component={Signin} />
       {/* <PrivateRoute exact path='/user/edit/:userId' component={EditProfile} />
       <PrivateRoute exact path='/findpeople' component={FindPeople} />
       <PrivateRoute exact path='/user/:userId' component={Profile} /> */}
